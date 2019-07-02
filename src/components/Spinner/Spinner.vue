9bf03a9a5ca2ea6b361759fa9d07d9e687dd5a37
Yaroslav <yaroslav.afanassiev@gmail.com>
2019-07-02 18:37:46 +0300

    Update: spinner component

--- src/components/Spinner/Spinner.vue
+++ src/components/Spinner/Spinner.vue
@@ -1,18 +1,7 @@
 <template>
-  <div class="Spinner">
-    <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle class="length" fill="none" stroke-width="8" stroke-linecap="round" cx="33" cy="33" r="28"></circle>
-    </svg>
-    <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle fill="none" stroke-width="8" stroke-linecap="round" cx="33" cy="33" r="28"></circle>
-    </svg>
-    <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle fill="none" stroke-width="8" stroke-linecap="round" cx="33" cy="33" r="28"></circle>
-    </svg>
-    <svg viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle fill="none" stroke-width="8" stroke-linecap="round" cx="33" cy="33" r="28"></circle>
-    </svg>
-  </div>
+  <svg class="Spinner" viewBox="0 0 50 50">
+    <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+  </svg>
 </template>
 
 <script>
