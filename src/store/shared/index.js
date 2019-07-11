280bb4785cd37e17c5668e858a532b9f60a46171
Yaroslav <yaroslav.afanassiev@gmail.com>
2019-07-11 18:21:55 +0300

    Add: load genres to reducer

--- src/store/shared/index.js
+++ src/store/shared/index.js
@@ -4,6 +4,7 @@ export default {
   state: {
     loading: false,
     configuration: null,
+    genres: null,
     error: null
   },
   mutations: {
@@ -18,7 +19,10 @@ export default {
     },
     setConfiguration (state, payload) {
       state.configuration = payload;
-    }
+    },
+    setGenres (state, payload) {
+      state.genres = payload;
+    },
   },
   actions: {
     clearError ({commit}) {
@@ -33,11 +37,32 @@ export default {
       }).catch(error => {
         commit('setError', error)
       });
+    },
+    async setGenres ({commit}) {
+      const genres = {
+        tv: [],
+        movies: []
+      };
+      await axios.get('https://api.themoviedb.org/3/genre/movie/list').then(response => {
+        genres.movies = response.data.genres
+      }).catch(error => {
+        commit('setError', error)
+      });
+      await axios.get('https://api.themoviedb.org/3/genre/tv/list').then(response => {
+        genres.tv = response.data.genres
+      }).catch(error => {
+        commit('setError', error)
+      });
+
+      commit('setGenres', genres)
     }
   },
   getters: {
     configuration (state) {
       return state.configuration
+    },
+    genres (state) {
+      return state.genres
     },
     loading (state) {
       return state.loading
