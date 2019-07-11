f421ca3c230a9c407a8ade7e97a5ebae256c0d7e
Yaroslav <yaroslav.afanassiev@gmail.com>
2019-07-11 16:00:07 +0300

    Add: hover details to movie list item

--- src/components/MovieListItem/MovieListItem.vue
+++ src/components/MovieListItem/MovieListItem.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="MovieListItem" :style="{ backgroundImage: 'url(' + getImageUrl(movie.poster_path, 3) + ')' }"></div>
+  <div class="MovieListItem" :style="{ backgroundImage: 'url(' + getImageUrl(movie.poster_path, 3) + ')' }">
+    <div class="MovieListItem__details">
+      <h3 class="MovieListItem__title">
+        {{!!movie.title ? movie.title : movie.name}}
+      </h3>
+      <p class="MovieListItem__description">
+        {{movie.overview}}
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
