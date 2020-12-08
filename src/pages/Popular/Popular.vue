9a87ee8d9faa2450951c8427ad2f51cff625338e
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-12-09 00:24:40 +0200

    Add: popular movies page

---
+++ src/pages/Popular/Popular.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="page__content">
+    <h1>Popular</h1>
+    <MovieList request-url="movie/popular" />
+  </div>
+</template>
+
+<script>
+  import MovieList from '../../components/MovieList/MovieList';
+
+  export default {
+    components: {
+      MovieList,
+    },
+  }
+</script>
