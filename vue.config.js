c29d812bc7b72ccc3bbc9660769a48facbb43390
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-12-09 00:53:11 +0200

    Install: vue-svg-loader package

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   runtimeCompiler: true,
   productionSourceMap: false,
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg');
+ 
+    svgRule.uses.clear();
+ 
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [{ removeViewBox: false }]
+        }
+      });
+  },
 };
