92edc0455560599bb8c4831b0e59d9b14bf43c38
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-12-09 00:10:33 +0200

    Refactor: Movies page component

--- src/pages/Movies/Movies.vue
+++ src/pages/Movies/Movies.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="page__content">
     <h1>Movies <span v-if="categoryTitle">({{categoryTitle}})</span></h1>
-    <MovieList :request-url="'discover/movie?with_genres=' + movieGenresId"></MovieList>
+    <MovieList :request-url="'discover/movie?with_genres=' + movieGenresId" />
   </div>
 </template>
 
 <script>
-  import MovieList from '../../components/MovieList/MovieList'
+  import MovieList from '../../components/MovieList/MovieList';
 
   export default {
     components: {
       MovieList,
     },
-    data () {
+    data() {
       return {
         categoryTitle: '',
         movieGenresId: +this.$route.params.id,
-      }
+      };
     },
     mounted() {
-      if (!!this.$store.getters.genres) {
-        this.setTitle()
+      if (this.$store.getters.genres) {
+        this.setTitle();
       }
     },
     computed: {
-      query () {
-        return this.$route.params.id
+      query() {
+        return this.$route.params.id;
       },
       genres() {
         return this.$store.getters.genres;
       },
     },
     watch: {
-      query (value) {
+      query(value) {
         if (value !== null && value !== undefined) {
           this.movieGenresId = +this.$route.params.id;
           this.setTitle();
@@ -42,16 +42,12 @@
         if (value !== null && value !== undefined) {
           this.setTitle();
         }
-      }
+      },
     },
     methods: {
-      setTitle () {
-        this.categoryTitle = this.genres.movies.find(item => item.id === this.movieGenresId).name
+      setTitle() {
+        this.categoryTitle = this.genres.movies.find(({ id }) => id === this.movieGenresId).name;
       },
-    }
+    },
   }
 </script>
-
-<style lang="scss">
-  @import "Movies";
-</style>
