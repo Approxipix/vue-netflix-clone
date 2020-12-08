9f4df623debf33fbaa2704a1c3d64d1d41d3a4da
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-12-09 00:29:56 +0200

    Refactor: MovieSlider component

--- src/components/MovieSlider/MovieSlider.vue
+++ src/components/MovieSlider/MovieSlider.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="MovieSlider__wrapper">
-    <h2 class="MovieSlider__title">{{categoryTitle}}</h2>
+    <h2 class="MovieSlider__title">{{ categoryTitle }}</h2>
     <div v-click-outside="clickOut">
       <Slider ref="slider" :options="options" :class="{'Slider--has-selected': isOpened && selectedMovie}">
         <div :key="index" v-for="(movie,index) in movieList" :class="`slide--${index}`">
-          <MovieSliderItem :movie="movie" :isOpened="isOpened" v-on:select-movie="selectMovie"></MovieSliderItem>
+          <MovieSliderItem :movie="movie" :isOpened="isOpened" v-on:select-movie="selectMovie" />
         </div>
       </Slider>
-      <MoviePreviewModal v-if="isOpened"></MoviePreviewModal>
+      <div v-if="isOpened" class="MovieSlider__details">
+        <MovieDetails v-if="isOpened" :movie="selectedMovie" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
+  import axios from 'axios';
   import Slider from '../../components/Slider/Slider.vue';
   import MovieSliderItem from '../../components/MovieSliderItem/MovieSliderItem.vue';
-  import MoviePreviewModal from '../../components/MoviePreviewModal/MoviePreviewModal.vue';
+  import MovieDetails from '../../components/MovieDetails/MovieDetails.vue';
   import clickOutside from '../../helpers/directives/clickOutside';
 
   export default {
@@ -25,10 +27,15 @@
       categoryTitle: String,
       requestUrl: String,
     },
+    components: {
+      Slider,
+      MovieSliderItem,
+      MovieDetails,
+    },
     directives: {
       clickOutside,
     },
-    data () {
+    data() {
       return {
         movieList: [],
         isOpened: false,
@@ -38,10 +45,10 @@
           slidesToShow: 5,
           slidesToScroll: 5,
           speed: 300,
-          timing: "ease-in-out",
-          extraClass: 'MovieSlider'
+          timing: 'ease-in-out',
+          extraClass: 'MovieSlider',
         }
-      }
+      };
     },
     computed: {
       selectedMovie() {
@@ -49,36 +56,39 @@
       },
     },
     methods: {
-      selectMovie (movie) {
-        this.toggleModal(true);
-        this.$store.dispatch('selectMovie', movie)
+      selectMovie(movie) {
+        if (this.selectedMovie && this.selectedMovie.id === movie.id) {
+          this.$store.dispatch('selectMovie', null);
+          this.toggleModal(false);
+        } else {
+          this.$store.dispatch('selectMovie', movie);
+          this.toggleModal(true);
+        }
       },
-      toggleModal (state) {
+      toggleModal(state) {
         this.isOpened = state !== undefined ? state : !this.isOpened;
       },
-      clickOut () {
-        this.toggleModal(false)
+      clickOut() {
+        this.toggleModal(false);
       }
     },
     mounted() {
       this.$refs.slider.toggleLoading();
-      axios.get(`https://api.themoviedb.org/3/${this.requestUrl}`).then((response) => {
-        this.movieList = response.data.results;
-      }).then(() => {
-        this.$refs.slider.reload();
-        this.$refs.slider.toggleLoading();
-      }).catch((error) => {
-        console.log(error)
-      })
+      axios.get(`https://api.themoviedb.org/3/${this.requestUrl}`)
+        .then(response => {
+          this.movieList = response.data.results;
+        })
+        .then(() => {
+          this.$refs.slider.reload();
+          this.$refs.slider.toggleLoading();
+        })
+        .catch(error => {
+          console.log(error);
+        });
     },
-    components: {
-      Slider,
-      MovieSliderItem,
-      MoviePreviewModal,
-    }
   }
 </script>
 
 <style lang="scss">
-  @import "MovieSlider";
+  @import 'MovieSlider';
 </style>
