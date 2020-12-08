a13a34133ff691a13fc08d94650f1b392b0a0a49
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-12-09 00:26:18 +0200

    Refactor: Movie Details compomnent

--- src/components/MovieDetails/MovieDetails.vue
+++ src/components/MovieDetails/MovieDetails.vue
@@ -1,43 +1,62 @@
 <template>
-  <div class="MovieDetails" :style="{ backgroundImage: 'url(' + getImageUrl(movie.backdrop_path, 2) + ')' }">
-    <div class="MovieDetails__fade--top"></div>
+  <div class="MovieDetails" :style="{ backgroundImage: getBackgroundImageUrl(movie.backdrop_path, 2) }">
+    <div class="MovieDetails__fade--top" />
     <div class="MovieDetails__wrapper">
       <h1 class="MovieDetails__title">
-        {{movie.title}}
+        {{ movie.title || movie.name }}
       </h1>
+      <div class="MovieDetails__labels">
+        <span class="MovieDetails__rate">{{ rate }} Match</span>
+        <span class="MovieDetails__date">{{ releaseDate }}</span>
+      </div>
       <div class="MovieDetails__details">
-        <div class="MovieDetails__actions">
-          <button type="button" class="btn MovieDetails__btn">
-            <font-awesome-icon :icon="['fas', 'play']" class="MovieDetails__btn-icon"/> Play
-          </button>
-          <button type="button" class="btn MovieDetails__btn">
-            <font-awesome-icon :icon="['fas', 'plus']" class="MovieDetails__btn-icon"/> My List
-          </button>
-        </div>
         <p class="MovieDetails__description">
-          {{movie.overview}}
+          {{ movie.overview }}
         </p>
+        <p class="MovieDetails__description">
+          Geners: <span class="MovieDetails__description--white">{{ genres }}</span>
+        </p>
+        <button type="button" class="btn MovieDetails__btn">
+          <font-awesome-icon :icon="['fas', 'plus']" class="MovieDetails__btn-icon" fixed-width /> My List
+        </button>
       </div>
     </div>
-    <div class="MovieDetails__fade--bottom"></div>
+    <div class="MovieDetails__fade--bottom" />
   </div>
 </template>
 
 <script>
-  import getImageUrl from '../../helpers/getImageUrl'
+  import getImageUrl from '../../helpers/getImageUrl';
 
   export default {
     props: {
       movie: Object,
     },
+    computed: {
+      rate() {
+        return `${this.movie.vote_average * 10}%`;
+      },
+      releaseDate() {
+        return new Date(this.movie.release_date || this.movie.first_air_date).toLocaleDateString();
+      },
+      genres() {
+        if (!this.$store.getters.genres) return '';
+        const movieGenres = this.$store.getters.genres.movies
+          .filter(({ id }) => this.movie.genre_ids.includes(id))
+          .map(({ name }) => name)
+          .join(', ');
+
+        return movieGenres;
+      }
+    },
     methods: {
-      getImageUrl: function (url, size) {
-        return getImageUrl(url, size, 'backdrop')
+      getBackgroundImageUrl(url, size) {
+        return `url(${getImageUrl(url, size, 'backdrop')})`;
       },
     },
   }
 </script>
 
 <style lang="scss">
-  @import "MovieDetails";
+  @import 'MovieDetails';
 </style>
