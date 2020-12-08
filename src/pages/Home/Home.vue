b1abad0ee5e829f20c34c4b1da19fa4fe7a79479
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-12-09 00:09:48 +0200

    Refactor: Home page component

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -3,37 +3,43 @@
     <div class="Home__main-slider">
       <Slider ref="slider" :options="options">
         <div v-for="(movie,index) in movieList" :key="index" :class="`slide--${index}`">
-          <MovieDetails :movie="movie"/>
+          <MovieDetails :movie="movie" />
         </div>
       </Slider>
     </div>
     <div class="Home__slider-list">
-      <MovieSlider category-title="Netflix Originals" request-url="discover/tv?with_networks=213"></MovieSlider>
-      <MovieSlider category-title="Trending Now" request-url="trending/all/week"></MovieSlider>
-      <MovieSlider category-title="Top Rated" request-url="movie/top_rated"></MovieSlider>
+      <MovieSlider category-title="Netflix Originals" request-url="discover/tv?with_networks=213" />
+      <MovieSlider category-title="Trending Now" request-url="trending/all/week" />
+      <MovieSlider category-title="Recently Added" request-url="movie/now_playing?with_networks=213" />
+      <MovieSlider category-title="Top Rated" request-url="movie/top_rated" />
     </div>
   </div>
 </template>
 
 <script>
-  import axios from 'axios'
+  import axios from 'axios';
   import Slider from '../../components/Slider/Slider.vue';
   import MovieDetails from '../../components/MovieDetails/MovieDetails.vue';
   import MovieSlider from '../../components/MovieSlider/MovieSlider.vue';
 
   export default {
+    components: {
+      Slider,
+      MovieDetails,
+      MovieSlider,
+    },
     data () {
       return {
         movieList: [],
         options: {
           dots: true,
-          // autoplay: true,
+          autoplay: true,
           slidesToShow: 1,
-          autoplaySpeed: 2000,
+          autoplaySpeed: 5000,
           speed: 300,
-          timing: "ease-in-out",
-        }
-      }
+          timing: 'ease-in-out',
+        },
+      };
     },
     computed: {
       user() {
@@ -43,23 +49,21 @@
     mounted() {
       this.$refs.slider.toggleLoading();
       this.$refs.slider.disableAutoPlay();
-      axios.get('https://api.themoviedb.org/3/discover/movie?region=US').then(response => {
-        this.movieList = response.data.results.splice(0, 10);
-      }).then(() => {
-        this.$refs.slider.reload();
-        this.$refs.slider.toggleLoading();
-      }).catch(error => {
-        console.log(error)
-      });
+      axios.get('https://api.themoviedb.org/3/discover/movie?region=US')
+        .then(response => {
+          this.movieList = response.data.results.splice(0, 10);
+        })
+        .then(() => {
+          this.$refs.slider.reload();
+          this.$refs.slider.toggleLoading();
+        })
+        .catch(error => {
+          console.log(error);
+        });
     },
-    components: {
-      Slider,
-      MovieDetails,
-      MovieSlider,
-    }
   };
 </script>
 
 <style lang="scss">
-  @import "Home";
+  @import 'Home';
 </style>
