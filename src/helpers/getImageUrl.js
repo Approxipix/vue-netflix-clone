14ff47077ffb2afcb4f1db5e4cb12440c8a76078
Yaroslav Afanasiev <yaroslav.afanassiev@gmail.com>
2020-12-16 03:28:05 +0200

    Update: format getImageUrl

--- src/helpers/getImageUrl.js
+++ src/helpers/getImageUrl.js
@@ -4,9 +4,9 @@ function getImageUrl(url, size = 3, type) {
   const config = store.getters.configuration;
   if (!config) return null;
   if (type === 'backdrop') {
-    return config.images.base_url + config.images.backdrop_sizes[size] + url
+    return config.images.base_url + config.images.backdrop_sizes[size] + url;
   } else {
-    return config.images.base_url + config.images.poster_sizes[size] + url
+    return config.images.base_url + config.images.poster_sizes[size] + url;
   }
 }
 
